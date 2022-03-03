9f60df05577d5f973e927fda36ff24d5da6300a5
GtlFrank <frankgtl1204@gmail.com>
2022-03-03 08:30:28 -0500

    esDiezOCinco

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -46,7 +46,16 @@ function saludo(idioma) {
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
   if (idioma === 'aleman') {
-    
+    return 'Guten Tag!'
+  }
+  else if (idioma === 'mandarin') {
+    return 'Ni Hao!'
+  }
+  else if (idioma === 'ingles') {
+    return 'Hello!'
+  }
+  else {
+    return 'Hola!'
   }
 }
 
@@ -58,12 +67,32 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+  switch (color) {
+    case 'blue': 
+      return 'This is blue'
+      break;
+    case 'red':
+      return 'This is red'
+      break;
+    case 'green':
+      return 'This is green'
+      break;
+    case 'orange':
+      return 'This is orange'
+      break;
+    default:
+      return 'Color not found';
+  }
 }
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero === 10 || numero === 5) {
+    return true;
+  }
+  return;
 }
 
 function estaEnRango(numero) {
